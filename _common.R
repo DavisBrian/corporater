075ed43c8a69dc8d33b6effb95f3b49df7cc43c3
Davis <bdavis07@hosp.dhha.org>
2018-04-26 16:38:26 -0600

    Preamble and Base R Basics pass 1 complete

---
+++ _common.R
@@ -0,0 +1,17 @@
+set.seed(31415)
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = FALSE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold",
+  dev = "svg"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+Sys.setenv(LANGUAGE = "en")
